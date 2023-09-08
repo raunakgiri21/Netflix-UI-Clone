2bf74c4d7faf2193a32018998b5b35620a494c5c
Raunak <raunakgiri21@gmail.com>
2023-09-08 12:22:51 +0530

    Merged client into server for deployment

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -3,12 +3,22 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
 const dotenv = require("dotenv");
+const path = require("path");
+
+const __currDirname = path.resolve();
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__currDirname, "./netflix-ui/build")));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__currDirname, "./netflix-ui/build/index.html"), (err) => {
+    res.status(500).send(err);
+  });
+});
 
 const connect = () => {
   mongoose
